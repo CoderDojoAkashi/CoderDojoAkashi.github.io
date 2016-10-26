0ce6e96a48c14dcd6372170b5c94a9857e6fc734
Tsuyoshi Tatsukawa <ts.tatsukawa@gmail.com>
2016-10-27 01:21:59 +0900

    update build.js that build all *.ejs file

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -6,22 +6,17 @@ const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
 const ejs = require('gulp-ejs');
 const config = require('../config').simple;
-const index = './src/index.ejs';
-const template = './src/template.ejs';
+const files = './src/*.ejs';
 
 gulp.task('build', () => {
-    gulp.src(index)
-      .pipe(plumber())
-      .pipe(ejs({
-      }))
-      .pipe(rename("index.html"))
-      .pipe(gulp.dest(config.ejs.dest));
-   gulp.src(template)
-      .pipe(plumber())
-      .pipe(ejs({
-      }))
-      .pipe(rename("template.html"))
-      .pipe(gulp.dest(config.ejs.dest));
-
+    gulp.src(
+    [
+    'src/*.ejs', //入力ソースパターン&path
+    '!' + "src/_*.ejs" //入力除外ソースパターン&path
+    ])
+    .pipe(plumber())
+    .pipe(ejs())
+    .pipe(rename({extname: '.html'}))
+    .pipe(gulp.dest(config.ejs.dest));
 });
 
