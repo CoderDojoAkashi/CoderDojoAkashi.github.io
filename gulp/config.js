67314fc8bf06ac312bd930a0bc2f574b71dbbe1a
Tsuyoshi Tatsukawa <ts.tatsukawa@gmail.com>
2016-10-26 23:33:45 +0900

    update gulp files

--- gulp/config.js
+++ gulp/config.js
@@ -1,14 +1,12 @@
 var baseConfig = {
   ejs: {
-      dest: 'public/'
+      dest: './'
   }
 };
 
 module.exports = {
-    /* 特別な定義が不要なタスクの設定 */
     simple: baseConfig,
 
-    /* copyタスクの設定 */
     copy: {
         base: baseConfig
 
--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -6,22 +6,14 @@ const plumber = require( 'gulp-plumber' );
 const rename = require( 'gulp-rename' );
 const ejs = require( 'gulp-ejs' );
 const config = require('../config').simple;
-const jsonData = 'pages.json';
-const template = 'template.ejs';
-const json = JSON.parse(fs.readFileSync( jsonData ));
-const pages = json.pages;
+const template = './src/template.ejs';
 
 gulp.task( 'build', () => {
-    for (let i = 0; i < pages.length; i++) {
-        const id = pages[i].id;
-
         gulp.src( template )
             .pipe( plumber() )
             .pipe(ejs({
-                jsonData: pages[i]
             }))
-        .pipe( rename(id + ".html") )
+        .pipe( rename("template.html") )
             .pipe(gulp.dest( config.ejs.dest ));
-    }
 });
 
