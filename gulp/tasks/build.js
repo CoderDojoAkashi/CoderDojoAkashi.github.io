1994f676ee5862beaaa1fcb9c14092b3da588396
Tsuyoshi Tatsukawa <ts.tatsukawa@gmail.com>
2016-10-27 00:59:07 +0900

    fix bugs

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,19 +1,27 @@
 'use strict'
 
-const gulp = require( 'gulp' );
-const fs = require( 'fs' );
-const plumber = require( 'gulp-plumber' );
-const rename = require( 'gulp-rename' );
-const ejs = require( 'gulp-ejs' );
+const gulp = require('gulp');
+const fs = require('fs');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const ejs = require('gulp-ejs');
 const config = require('../config').simple;
+const index = './src/index.ejs';
 const template = './src/template.ejs';
 
-gulp.task( 'build', () => {
-        gulp.src( template )
-            .pipe( plumber() )
-            .pipe(ejs({
-            }))
-        .pipe( rename("template.html") )
-            .pipe(gulp.dest( config.ejs.dest ));
+gulp.task('build', () => {
+    gulp.src(index)
+      .pipe(plumber())
+      .pipe(ejs({
+      }))
+      .pipe(rename("index.html"))
+      .pipe(gulp.dest(config.ejs.dest));
+   gulp.src(template)
+      .pipe(plumber())
+      .pipe(ejs({
+      }))
+      .pipe(rename("template.html"))
+      .pipe(gulp.dest(config.ejs.dest));
+
 });
 
